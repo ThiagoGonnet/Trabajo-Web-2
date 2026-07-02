fc94e4c2fe308402eb05c694c496c354315464b0
Tomas Gonzalez <tomasgonzalez429030@gmail.com>
2026-07-02 13:59:09 -0300

    actualizo vista

--- app/views/AuthView.php
+++ app/views/AuthView.php
@@ -9,7 +9,9 @@ class AuthView
   public function mostrarPanelAdmin($libros, $autores)
   {
     require_once "./app/views/templates/layouts/header.phtml";
-    require_once "./app/views/templates/panel-categorias.phtml";
+    require_once "./app/views/templates/form_agregarAutor.phtml";
+    require_once "./app/views/templates/form_eliminarAutor.phtml";
+    require_once "./app/views/templates/form_actualizarAutor.phtml";
     require_once "./app/views/templates/form-subir-libro.phtml";
     require_once "./app/views/templates/form-eliminar-libro.phtml";
     require_once "./app/views/templates/form-actualizar-libro.phtml";
